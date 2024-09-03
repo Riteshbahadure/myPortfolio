e9b0c03bff9fdc6aa11fa31433b34cb10a49100e
Rutuja Bahadure <bahadurerutu558@gamil.com>
2024-09-03 13:52:33 +0530

    server

---
+++ routes/authRoute.js
@@ -0,0 +1,10 @@
+const { registerUser, loginUser, logoutUser } = require("../controller/auth.Controller")
+
+const router = require("express").Router()
+
+router
+    .post("/register", registerUser)
+    .post("/login", loginUser)
+    .post("/logout", logoutUser)
+
+module.exports = router
